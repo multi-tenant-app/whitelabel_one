261855c2906e5106f863291b5e0c6d4f93655d35
Sascha Turowski <turowski@turtec.de>
2018-06-28 17:16:57 +0200

    first commit

---
+++ android/app/src/main/java/com/whitelabel_one/MainActivity.java
@@ -0,0 +1,15 @@
+package com.whitelabel_one;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "whitelabel_one";
+    }
+}
